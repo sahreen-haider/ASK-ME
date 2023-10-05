da4e499482da90c61d54d198502e0523a29e146f
Sahreen Haider <81517526+sahreen-haider@users.noreply.github.com>
2023-10-05 16:32:18 +0530

    Add files via upload

---
+++ langgo.py
@@ -0,0 +1,34 @@
+import os
+# from langchain_back import *
+from credenti import *
+from langchain.llms import OpenAI
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.memory import ConversationBufferMemory
+import streamlit as st
+import pornhub
+
+
+os.environ['OPENAI_API_KEY'] = OPENAI_KEY
+os.environ['SERPAPI_API_KEY'] = SERPAPI_KEY
+
+
+st.title(':rainbow[ASK ME]')
+
+llm = OpenAI(temperature=0.9)
+
+memory = ConversationBufferMemory()
+
+tools = load_tools(['serpapi', 'llm-math'], llm=llm)
+
+agent = initialize_agent(
+    tools,
+    llm,
+    agent = AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    memory = memory,
+    verbose = False
+)
+prompt = st.text_input('**Please enter your Prompt here:** ')
+resultant = agent.run(prompt)
+st.write(resultant)
+
+
