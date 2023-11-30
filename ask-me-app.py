8a1dabb55b52a85e916cf89db4bcaf070f784fae
sahreen-haider <sahreenhaider@gmail.com>
2023-11-30 23:09:54 +0530

    .

--- ask-me-app.py
+++ ask-me-app.py
@@ -7,11 +7,9 @@ import whisper
 import serpapi
 from rec import *
 
-OPENAI_KEY = "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"
-SERPAPI_KEY = "654b8bee0c754d8a87403bc292cffb8c2296190e6bf88f86f96a7f4d67d118cf"
 
-os.environ['OPENAI_API_KEY'] = OPENAI_KEY
-os.environ['SERPAPI_API_KEY'] = SERPAPI_KEY
+os.environ['OPENAI_API_KEY'] = st.secrets["OPENAI_KEY"]
+os.environ['SERPAPI_API_KEY'] = st.secrets["SERPAPI_KEY"]
 
 path_to_audio = '/Users/sahreenhaider/Documents/ASK-ME/data/prompt.mp3'
 
@@ -34,17 +32,15 @@ agent = initialize_agent(
 col1, col2 = st.columns(2)
 
 prompt = col1.text_input('Enter your Prompt here: ')
-
-if col2.button('speak'):
-    model = whisper.load_model('base')
-    result = model.transcribe()
-
-if col2.button('voice'):
-    st.write('recording.......')
-    record_audio(path_to_audio)
-    st.write('recording ended.......')
-    resultant = agent.run(transcribe_to_text(path_to_audio))
-    st.write(resultant)
-else:
-    agent.run(prompt)
+voice = col2.button('voice')
+if prompt:
+    result = agent.run(prompt)
     st.write(result)
+
+else:
+    if col2:
+        st.write('recording.......')
+        record_audio(7, path_to_audio)
+        st.write('recording ended.......')
+        resultant = agent.run(transcribe_to_text(path_to_audio))
+        st.write(resultant)
